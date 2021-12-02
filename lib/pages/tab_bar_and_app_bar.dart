5188323d443b122d0a2d82d86e968a1fd8166bfd
jimmy <jimmy.pang@bioserenity.cn>
2021-12-02 14:42:54 +0800

    tab bar demo

--- lib/pages/tab_bar_and_app_bar.dart
+++ lib/pages/tab_bar_and_app_bar.dart
@@ -15,12 +15,29 @@ class TabBarAndAppBar extends StatefulWidget {
 class TabBarAndAppBarState extends State<TabBarAndAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Text('jimmy'),
+    return DefaultTabController(
+      length: _choices.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: _choices
+                .map((item) => Tab(
+                      text: item.title,
+                      icon: Icon(item.icon),
+                    ))
+                .toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: _choices
+              .map((item) => Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: _ChoiceCard(tempChoice: item),
+                  ))
+              .toList(),
+        ),
       ),
     );
   }
@@ -40,3 +57,33 @@ const List<_Choice> _choices = <_Choice>[
   _Choice(title: 'Train', icon: Icons.directions_railway),
   _Choice(title: 'Walk', icon: Icons.directions_walk),
 ];
+
+class _ChoiceCard extends StatelessWidget {
+  final tempChoice;
+  const _ChoiceCard({Key? key, this.tempChoice}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final style = Theme.of(context).textTheme.bodyText1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              tempChoice!.icon,
+              size: 128.0,
+              color: style!.color,
+            ),
+            Text(
+              tempChoice!.title,
+              style: style,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
